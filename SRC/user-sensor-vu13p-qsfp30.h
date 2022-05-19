191893ca55f2f9fb8f7b7ae8d95b51e61cd7fe90
agreshil <algreshilov88@gmail.com>
2022-05-19 19:45:07 +0200

    Fixes

--- SRC/user-sensor-vu13p-qsfp30.h
+++ SRC/user-sensor-vu13p-qsfp30.h
@@ -28,42 +28,6 @@ typedef struct qsfp_select_s
     unsigned char bit_num; // 0..7 within byte
 } qsfp_select_t;
 
-// map of the qsfp selection bits.
-// qsfp module numbers in comments match schematics
-qsfp_select_t qsfp_select [30] =
-{
-    {0, 2, 7}, // QSFP 0
-    {0, 2, 4}, // QSFP 1
-    {0, 2, 2}, // QSFP 2
-    {0, 1, 7}, // QSFP 3
-    {0, 1, 5}, // QSFP 4
-    {0, 1, 2}, // QSFP 5
-    {1, 0, 0}, // QSFP 6
-    {1, 0, 2}, // QSFP 7
-    {1, 0, 4}, // QSFP 8
-    {1, 0, 6}, // QSFP 9
-    {1, 1, 0}, // QSFP 10
-    {1, 1, 2}, // QSFP 11
-    {1, 1, 4}, // QSFP 12
-    {1, 1, 6}, // QSFP 13
-    {0, 2, 6}, // QSFP 14
-    {0, 2, 3}, // QSFP 15
-    {0, 2, 1}, // QSFP 16
-    {0, 1, 6}, // QSFP 17
-    {0, 1, 4}, // QSFP 18
-    {0, 1, 1}, // QSFP 19
-    {1, 0, 1}, // QSFP 20
-    {1, 0, 3}, // QSFP 21
-    {1, 0, 5}, // QSFP 22
-    {1, 0, 7}, // QSFP 23
-    {1, 1, 1}, // QSFP 24
-    {1, 1, 3}, // QSFP 25
-    {1, 1, 5}, // QSFP 26
-    {1, 1, 7}, // QSFP 27
-    {1, 2, 0}, // QSFP 28
-    {1, 2, 1}  // QSFP 29
-};
-
 // read temperature
 union temperature
 {
